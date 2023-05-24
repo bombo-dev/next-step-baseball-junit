cf405148f8bf07133eb6289cc90ddf258eca8461
Bombo <bombo96@chungwoon.ac.kr>
2023-05-25 03:06:44 +0900

    test (set contains, used ParameterizedTest)

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -26,5 +28,20 @@ public class SetTest {
     @DisplayName("set sizeTest")
     void setSize() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("set contains()")
+    void setContains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("set contains used ParameterizedTest")
+    void setContainsParameterizedTest(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 }
